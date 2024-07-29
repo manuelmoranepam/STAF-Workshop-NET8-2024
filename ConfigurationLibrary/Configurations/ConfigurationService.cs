f6eccf63b0a1ab02803f7739ebba39012880b24d
Manuel Moran <manuel.moran@arrivia.com>
2024-07-29 15:48:57 -0700

    Initial commit, solution created, configuration library project created, gitignore file added.

---
+++ ConfigurationLibrary/Configurations/ConfigurationService.cs
@@ -0,0 +1,54 @@
+﻿using ConfigurationLibrary.Interfaces.Configurations;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+
+namespace ConfigurationLibrary.Configurations;
+
+public class ConfigurationService : IConfigurationService
+{
+	private readonly IConfiguration _configuration;
+
+	public ConfigurationService(string filePath)
+	{
+		ArgumentException.ThrowIfNullOrWhiteSpace(filePath);
+
+		_configuration = new ConfigurationBuilder()
+			.SetBasePath(Directory.GetCurrentDirectory())
+			.AddJsonFile(filePath, false, true)
+			.Build();
+	}
+
+	public IConfiguration GetConfiguration()
+	{
+		return _configuration;
+	}
+
+	public T GetConfigurationSection<T>(string sectionName)
+	{
+		ArgumentException.ThrowIfNullOrWhiteSpace(sectionName);
+
+		var section = _configuration.GetSection(sectionName).Get<T>();
+
+		if (Equals(section, default(T)) || section is null)
+		{
+			throw new ArgumentException($"Section {sectionName} not found in configuration file.");
+		}
+
+		return section;
+	}
+
+	public T GetConfigurationValue<T>(string key)
+	{
+		ArgumentException.ThrowIfNullOrWhiteSpace(key);
+
+		var value = _configuration.GetValue<T>(key);
+
+		if (Equals(value, default(T)) || value is null)
+		{
+			throw new ArgumentException($"Key {key} not found in configuration file.");
+		}
+
+		return value;
+	}
+}
---
+++ ConfigurationLibrary/Interfaces/Configurations/IConfigurationService.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace ConfigurationLibrary.Interfaces.Configurations;
+
+public interface IConfigurationService
+{
+	IConfiguration GetConfiguration();
+	T GetConfigurationSection<T>(string sectionName);
+	T GetConfigurationValue<T>(string key);
+}
